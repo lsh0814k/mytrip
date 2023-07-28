7a1b144af0ea74d83d9d88a3ef46a6d35c9d279c
slee <slee@magicsoft.co.kr>
2023-07-28 10:17:13 +0900

    first commit

---
+++ src/test/java/toy/mytrip/MytripApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.mytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
